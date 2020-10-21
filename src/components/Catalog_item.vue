d7dcaae8f2cc0ab44f9c82f70e952f3bd435f0b6
EVG <evg@mail.ru>
2020-10-22 02:29:38 +0300

    22-10-2020

---
+++ src/components/Catalog_item.vue
@@ -0,0 +1,75 @@
+<template>
+
+<div class = "catalog__item">
+
+    <div class = "catalog__pic" :style = "{backgroundImage: `url('.pics/img/bouquets/1/1.jpg')`}"></div>
+
+    <div class = "catalog__title">{{items.title}}</div>
+    <div class = "catalog__price">{{items.price}}</div>
+
+    <div class = "catalog__text">{{items.title}}</div>
+
+
+</div>
+
+</template>
+
+
+<script>
+
+export default {
+
+    name: 'Catalog_item',
+    props: {
+        items: {
+            type: Object,
+            default(){}
+        }
+    },
+    data(){
+        return {}
+    },
+    methods: {
+
+
+    }
+}
+
+</script>
+
+<style scoped>
+
+.catalog__item {
+    border: 1px solid red;
+    box-sizing: border-box;
+    flex-basis: 250px;
+    min-height: 200px;
+    margin-bottom: 25px;
+}
+
+.catalog__pic {
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 150px;
+}
+
+.catalog__title {
+    text-align: center;
+    margin: 10px;
+    font-weight: bold;
+}
+
+.catalog__price {
+    margin: 20px 0;
+    text-align: center;
+    font-weight: bold;
+}
+
+.catalog__text {
+
+    text-align: left;
+}
+
+
+
+</style>
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,21 +1,26 @@
 <template>
-
 <div>
-каталог
+<button class = "get-items" @click = "addPreloader()">Получить товары</button>
+<div class = "catalog">
 
-<button class = "get-items" @click = "getProducts()">Получить товары</button>
+
+<Catalog_item
+    v-for = 'item in items'
+    :key = 'item.id'
+    v-bind:items = 'item'
+/>
+
+
+</div>
 
 </div>
 
 </template>
 
-<style scoped>
-
-</style>
-
 <script>
 
 //import axios from 'axios'
+import Catalog_item from '../components/Catalog_item'
 
 export default {
 
@@ -23,8 +28,7 @@ export default {
         return {}
     },
     components: {
-
-
+        Catalog_item
     },
     computed: {
         items(){
@@ -35,13 +39,69 @@ export default {
     methods: {
 
         getProducts(){
+            // Удалить этот метод
             console.log(this.items)
+        },
+        getMaxObjId(){
+            let maxObjId = 0    // Поиск наибольшего id из объекта товаров каталога в хранилище
+            for (let k in this.items) {
+                maxObjId < this.items[k].id ? maxObjId = this.items[k].id : ''
+            }
+            return maxObjId
+        },
+        addPreloader(){
+
+            let preloader = document.createElement('img')
+                preloader.classList.add('catalog-preloader')
+                preloader.setAttribute('src', '/assets/icons/2.gif')
+
+            //preloader.insertAdjacentElement('beforeEnd', document.querySelector('.catalog'))
+            //preloader.insertBefore(document.querySelector('.catalog'))
+            document.querySelector('.catalog').append(preloader)
+        },
+
+        getMoreCatalogItems(){
+
+            let items = document.querySelectorAll('.catalog__item')
+
+            if (this.isInViewport(document.querySelectorAll('.catalog__item')[items.length - 10])) {
+                window.removeEventListener('scroll', this.getMoreCatalogItems)
+                this.addPreloader()
+            }
+
+        },
+        isInViewport(element) {
+            let rect = element.getBoundingClientRect();
+            let html = document.documentElement;
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || html.clientHeight) &&
+                rect.right <= (window.innerWidth || html.clientWidth)
+            )
         }
     },
   created() {
+    window.addEventListener('scroll', this.getMoreCatalogItems)
 
     this.$store.dispatch('get_catalog')
   }
 }
 
 </script>
+
+<style scoped>
+
+.catalog {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.catalog::after {
+  content: "";
+  flex-basis: 250px;
+}
+
+
+</style>
