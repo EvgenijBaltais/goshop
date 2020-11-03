08346e0de67593afb43c635bc9f8d0ee53a8d868
Evgeny <evgenij.baltais@gmail.com>
2020-11-03 19:55:36 +0300

    03-11-2020

--- src/views/Catalog_item.vue
+++ src/views/Catalog_item.vue
@@ -2,13 +2,14 @@
     
 <div class = "catalog-item">
     <div class = "product-carousel">
-        <div class="product-slider">11
-
+        <div class="product-slider">
             <div class="product-slider__mainview">
-
+                <img src="" alt="" class = "product-slider__bimg">
             </div>
             <div class="product-slider__navigation">
-
+                <img src="" alt="" class = "product-slider__simg">
+                <img src="" alt="" class = "product-slider__simg">
+                <img src="" alt="" class = "product-slider__simg">
             </div>
         </div>
     </div>
@@ -27,12 +28,13 @@
             <p class = "product-text__p">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, laborum minus et nesciunt qui voluptate quis ipsa voluptatem soluta sed, ad iure provident aspernatur corrupti odit dolore sint quas dicta.</p>
         </div>
 
-        <div class="product-contains">
+        <div class="product-contains" v-on:click = "openContainer()">
             <div class="product-contains__title">
-
+                <span>Состав букета</span>
+                <img src="../assets/icons/to-bottom.svg" alt="" class = "to-bottom-arrow">
             </div>
             <div class="product-contains__inside">
-                
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consequuntur numquam fuga! Dolorum sapiente necessitatibus, dicta totam eaque sit laborum quasi quas, deserunt natus saepe fugiat. Molestiae praesentium accusantium eius?
             </div>
         </div>
     </div>
@@ -54,6 +56,24 @@
 .product-carousel {
     width: calc(50% - 30px);
     outline: 1px solid green;
+}
+
+.product-slider__bimg {
+    display: block;
+}
+
+.product-slider__simg {
+    display: block;
+    width: 60px;
+    margin-right: 10px;
+}
+
+.product-slider__simg:last-child {
+    margin-right: 0;
+}
+
+.product-slider__navigation {
+    display: flex;
 }
 
 .product-description {
@@ -87,12 +107,49 @@
     margin: 10px 0;
     border-top: 1px solid #E4E4E4;
     border-bottom: 1px solid #E4E4E4;
+    padding: 10px;
 }
 
 .product-contains__title {
     display: flex;
+    position: relative;
+    padding-right: 30px;
+    box-sizing: border-box;
+    font-size: 18px;
+    line-height: 30px;
+    cursor: pointer;
+    user-select: none;
 }
 
+.product-contains-opened .product-contains__title {
+    font-weight: bold;
+}
+
+.to-bottom-arrow {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+    display: block;
+    width: 14px;
+    transition: transform .3s;
+}
+
+.product-contains-opened .to-bottom-arrow {
+    transform: rotate(180deg);
+    transition: transform .3s;
+}
+
+.product-contains__inside {
+    padding-top: 10px;
+    box-sizing: border-box;
+    display: none;
+}
+
+.product-contains-opened .product-contains__inside {
+    display: block;
+}
 
 </style>
 
@@ -102,6 +159,25 @@ export default {
     name: 'Catalog_item',
     data(){
         return {}
+    },
+    methods: {
+
+        openContainer: function(){
+
+        if (!(event.target.classList.contains('product-contains') || this.getParent(event.target, 'product-contains'))) {
+           return false
+        }
+
+        let productContains = document.querySelector('.product-contains')
+
+            productContains.classList.contains('product-contains-opened') ? 
+            productContains.classList.remove('product-contains-opened') : 
+            productContains.classList.add('product-contains-opened')
+        },
+        getParent: function(el, cls){
+            while ((el = el.parentElement) && !el.classList.contains(cls));
+            return el;
+        }
     }
 }
 </script>
